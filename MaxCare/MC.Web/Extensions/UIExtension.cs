6d6fef229a39f065a1fb1bbd348f956754e067f3
Vincent Pantia <vincent.m.pantia@hotmail.com>
2025-01-05 18:47:43 +0800

    Minor: Add UI Extension class.

---
+++ MaxCare/MC.Web/Extensions/UIExtension.cs
@@ -0,0 +1,16 @@
+﻿using AntDesign;
+using MC.Web.Models.UI;
+
+namespace MC.Web.Extensions
+{
+    public class UIExtension
+    {
+        public static IEnumerable<SidebarItem> GetSidebarItems() =>
+            new List<SidebarItem>
+            {
+                new SidebarItem("1", IconType.Outline.Home, "", "Home"),
+                new SidebarItem("2", IconType.Outline.User, "counter", "Counter"),
+                new SidebarItem("3", IconType.Outline.User, "weather", "Weather"),
+            };
+    }
+}
